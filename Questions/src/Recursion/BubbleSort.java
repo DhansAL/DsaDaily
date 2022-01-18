763ced1d3dd821a993ff77161772e0312b045ac5
Dhans <sgdhans@gmail.com>
2022-01-18 18:05:25 +0530

    recursion strlength 9

---
+++ Questions/src/Recursion/BubbleSort.java
@@ -0,0 +1,8 @@
+package Recursion;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+
+    }
+    static int bubblesort
+}
--- Questions/src/Recursion/lengthstring.java
+++ Questions/src/Recursion/lengthstring.java
@@ -2,9 +2,19 @@ package Recursion;
 
 public class lengthstring {
     public static void main(String[] args) {
+        System.out.println(fn("hello",0));
+        System.out.println(fn2("hello"));
+    }
+    //lol approach
+    static int fn(String str,int n){
+    if(str == "")return n;
+    str =  str.substring(0, str.length() - 1);
+    return fn(str, n+1);
+    }
+    // better approach
+    static int fn2(String str){
+        if(str == "")return 0;
+       return fn2(str.substring(1))+1;
+    }
 
-    }
-    static int fn(String str){
-        
-    }
 }
